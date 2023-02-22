1b71fa0562236fad70a1a3a25d3c846a54c46390
Nathan Olive <65222446+Natolive@users.noreply.github.com>
2023-02-22 15:29:55 +0100

    allow cors (#2)

--- api/index.php
+++ api/index.php
@@ -1,5 +1,13 @@
 <?php
 require '../vendor/autoload.php';
+
+// ALLOW CORS
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Content-type: text/html; charset=UTF-8'); 
+header('Access-Control-Allow-Headers: X-Requested-With');
+
 require 'Model/constant.php';
 use Api\Controller\ControllerRouter;
 
